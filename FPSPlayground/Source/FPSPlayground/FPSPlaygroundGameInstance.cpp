2f7a633afcb77a0a56adc1b93a7af9a52088d3c8
Charles Thrift <kablammyjr96@gmail.com>
2018-10-14 21:36:23 -0400

    Can now join online servers through server list on steam servers

--- FPSPlayground/Source/FPSPlayground/FPSPlaygroundGameInstance.cpp
+++ FPSPlayground/Source/FPSPlayground/FPSPlaygroundGameInstance.cpp
@@ -117,7 +117,9 @@ void UFPSPlaygroundGameInstance::RefreshServerList()
 	SessionSearch = MakeShareable(new FOnlineSessionSearch());
 	if (SessionSearch.IsValid())
 	{
-		SessionSearch->bIsLanQuery = true;
+		//SessionSearch->bIsLanQuery = true;
+		SessionSearch->MaxSearchResults = 100;
+		SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
 		UE_LOG(LogTemp, Warning, TEXT("Starting to find sessions"));
 		SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
 	}
@@ -153,9 +155,10 @@ void UFPSPlaygroundGameInstance::CreateSession()
 	if (SessionInterface.IsValid())
 	{
 		FOnlineSessionSettings SessionSettings;
-		SessionSettings.bIsLANMatch = true;
+		SessionSettings.bIsLANMatch = false;
 		SessionSettings.NumPublicConnections = 2;
 		SessionSettings.bShouldAdvertise = true;
+		SessionSettings.bUsesPresence = true;
 
 		SessionInterface->CreateSession(0, SESSION_NAME, SessionSettings);
 	}
